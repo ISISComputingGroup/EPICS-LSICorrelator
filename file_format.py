d89968c2706b1a523fb4818f498b0895e241c41f
Alistair McGann <alistair.mcgann@tessella.com>
2020-02-28 13:50:19 +0000

    Add file schema

---
+++ file_format.py
@@ -0,0 +1,21 @@
+FILE_SCHEME = """{date}\t{time}
+Pseudo Cross Correlation
+Scattering angle:\t{scattering_angle}
+Duration (s):\t{duration}
+Wavelength (nm):\t{wavelength}
+Refractive index:\t{refractive_index}
+Viscosity (mPas):\t{viscosity}
+Temperature (K):\t{temperature}
+Laser intensity (mW):\t{laser_intensity}
+Average Count rate A (kHz):\t{avg_count_A}
+Average Count rate B (kHz):\t{avg_count_B}
+Intercept:\t1.0000
+Cumulant 1st\t-Inf
+Cumulant 2nd\t-Inf\tNaN
+Cumulant 3rd\t-Inf\tNaN
+
+Lag time (s)         g2-1
+{correlation_function}
+
+Count Rate History (KHz)  CR CHA / CR CHB
+{count_rate_history}"""
