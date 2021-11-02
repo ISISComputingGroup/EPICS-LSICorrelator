d251075087b0b22cd930913c75476a537192dea8
JackEAllen <jack.allen@stfc.ac.uk>
2021-11-02 14:01:35 +0000

    Update open source license

--- correlator_pcaspy.py
+++ correlator_pcaspy.py
@@ -1,10 +1,18 @@
-# ############################################################################
-# Experimental Controls Group :
-# https://github.com/ISISComputingGroup/EPICS-LSICorrelator
+# This file is part of the ISIS IBEX application.
+# Copyright (C) 2020-2021 Science & Technology Facilities Council.
+# All rights reserved.
 #
-# Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI
-# SPDX - License - Identifier: GPL-3.0-or-later
-# ############################################################################
+# This program is distributed in the hope that it will be useful.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this distribution.
+# EXCEPT AS EXPRESSLY SET FORTH IN THE ECLIPSE PUBLIC LICENSE V1.0, THE PROGRAM
+# AND ACCOMPANYING MATERIALS ARE PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND.  See the Eclipse Public License v1.0 for more details.
+#
+# You should have received a copy of the Eclipse Public License v1.0
+# along with this program; if not, you can obtain a copy from
+# https://www.eclipse.org/org/documents/epl-v10.php or
+# http://opensource.org/licenses/eclipse-1.0.php
 
 from __future__ import print_function, unicode_literals, division, absolute_import
 
