efee325451b2e86a71d2b055c954d2ed8e37c655
JamesKingWork <james.c.king@stfc.ac.uk>
2021-11-23 11:25:56 +0000

    Move out magic numbers and other values into config object

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ class Config:
     FIRMWARE_REVISION_DEFAULT = "4.0.0.3"
     ADDRESS_MACRO = "ADDR"
     DELTA_T = 0.0524288 # Magic number, seems to be time between measurements.
+    SLEEP_BETWEEN_MEASUREMENTS = 0.5
 
