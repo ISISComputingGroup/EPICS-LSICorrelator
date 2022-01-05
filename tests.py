7585514a68c11a95ac3a016942e4740e4b48087a
JackEAllen <jack.allen@stfc.ac.uk>
2022-01-05 09:04:40 +0000

    Fix pylint import ordering, add module and methd docstrings and disable invalid name check

--- tests.py
+++ tests.py
@@ -1,14 +1,17 @@
+"""
+Contains Unit Tests for LSI Correlator
+"""
+from tempfile import NamedTemporaryFile
 import unittest
 import numpy as np  # pylint: disable=import-error
-from tempfile import NamedTemporaryFile
+
 
 from correlator_driver_functions import LSiCorrelatorVendorInterface
 from pvdb import Records
 
 from test_utils import test_data
-from unittest.mock import patch
 
-# pylint: disable=line-too-long
+# pylint: disable=line-too-long, invalid-name
 
 macros = {
     "SIMULATE": "1",
@@ -23,6 +26,9 @@ class LSICorrelatorTests(unittest.TestCase):
     """
 
     def setUp(self):
+        """
+        Set up the test case
+        """
         self.driver = LSiCorrelatorVendorInterface(macros, simulated=True)
 
         self.mocked_api = self.driver.mocked_api
@@ -30,6 +36,9 @@ class LSICorrelatorTests(unittest.TestCase):
         self.mocked_api.disconnected = False
 
     def test_GIVEN_device_disconnected_WHEN_data_taken_THEN_device_reads_no_data_and_disconnected(self):
+        """
+        Test that the device reads no data when disconnected
+        """
         self.assertTrue(self.driver.is_connected)
         self.device.disconnected = True
 
@@ -39,12 +48,18 @@ class LSICorrelatorTests(unittest.TestCase):
         self.assertFalse(self.driver.is_connected)
 
     def test_GIVEN_device_connected_WHEN_data_taken_THEN_device_reads_has_data_and_connected(self):
+        """
+        Test that the device reads data when connected
+        """
         self.driver.take_data(0)
 
         self.assertTrue(self.driver.has_data)
         self.assertTrue(self.driver.is_connected)
 
     def test_GIVEN_device_connected_WHEN_data_taken_THEN_driver_updated_with_correlation_and_time_lags(self):
+        """
+        Test that the driver updates with the correlation and time lags
+        """
 
         self.mocked_api.corr = test_data.corr
         self.mocked_api.lags = test_data.lags
@@ -54,6 +69,9 @@ class LSICorrelatorTests(unittest.TestCase):
         self.assertTrue(np.allclose(self.driver.lags, test_data.lags_without_nans))
 
     def test_GIVEN_device_has_data_WHEN_data_retrieved_from_device_THEN_time_trace_made_and_no_nans_in_correlation(self):
+        """
+        Test that the time trace is made and no nans in correlation
+        """
         self.device.Correlation = test_data.corr
         self.device.Lags = test_data.lags
         self.device.TraceChA = test_data.trace_a
@@ -68,16 +86,25 @@ class LSICorrelatorTests(unittest.TestCase):
         self.assertTrue(np.allclose(trace_time, test_data.trace_time))
 
     def test_WHEN_data_taken_THEN_start_called(self):
+        """
+        Test that the start method is called when data is taken
+        """
         self.driver.take_data(0)
         self.device.start.assert_called_once()
 
     def test_WHEN_data_taken_AND_measurement_on_THEN_update_called_WHEN_measurement_off_THEN_update_not_called(self):
+        """
+        Test that the update method is called when data is taken and measurement is on
+        """
         starting_update_count = self.mocked_api.update_count
         self.driver.take_data(0)
         self.assertGreater(self.mocked_api.update_count, starting_update_count)
         self.assertFalse(self.mocked_api.update_called_when_measurement_not_on)
 
     def test_WHEN_save_data_THEN_metadata_written_AND_correlation_data_written_AND_traces_written(self):
+        """
+        Test that the metadata and correlation data are written and the traces are written
+        """
         # Gather some dummy data that matches the test_data.dat file
         self.device.Correlation = test_data.corr
         self.device.Lags = test_data.lags
